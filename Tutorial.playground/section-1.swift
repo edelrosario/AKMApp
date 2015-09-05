051e9034969914e9a9ddae1bc67a4b09ded4ee8d
Elaine Del Rosario <e.delrosario94@gmail.com>
2015-09-04 21:47:46 -0400

    derp

---
+++ Tutorial.playground/section-1.swift
@@ -0,0 +1,29 @@
+<<<<<<< Updated upstream
+=======
+class TipCalculator {
+    let total: Double
+    let taxPct: Double
+    let subtotal: Double
+    
+    init(total: Double, taxPct: Double) {
+        self.total = total
+        self.taxPct = taxPct
+        subtotal = total / (taxPct + 1)
+    }
+    
+    func calcTipWithTipPct(tipPct: Double) -> Double {
+        return subtotal * tipPct
+    }
+    
+    func printPossibleTips() {
+        let possibleTipsInferred = [0.15, 0.18, 0.20]
+        let possibleTipsExplicit: [Double] = [0.15, 0.18, 0.20]
+        for possibleTip in possibleTipsInferred {
+            println("\(possibleTip*100)%: \(calcTipWithTipPct(possibleTip))")
+        }
+    }
+}
+
+let tipCalc = TipCalculator(total: 33.25, taxPct: 0.06)
+tipCalc.printPossibleTips()
+>>>>>>> Stashed changes
